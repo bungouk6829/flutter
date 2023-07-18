be1b8c1699b4214bbc12425d1316dc762e0655dd
bungouk kim <bungouk6829@gmail.com>
2023-07-18 17:45:28 +0900

    '7.18'

---
+++ lib/screens/detail_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(centerTitle: true, title: Text('패키지 없이 R로 구현하는 심층 강화학습')),
+      body: Column(children: [
+        Image.network(
+            'https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FCuoqW%2Fbtq8uatukHu%2FO0VapTwcTTpV3T29lqMYd0%2Fimg.png'),
+        Padding(padding: EdgeInsets.all(3)),
+        Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+                width: MediaQuery.of(context).size.width * 0.8,
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text('패키지 없이 R로 구현하는 심층 강화학습',
+                          style: TextStyle(
+                              fontSize: 23, fontWeight: FontWeight.bold)),
+                    ),
+                    Text(
+                      '손으로 풀어보는 Q-Learning부터 R로 구현하는 심층 강화학습까지',
+                      style: TextStyle(fontSize: 18, color: Colors.grey),
+                    )
+                  ],
+                )),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.15,
+              padding: EdgeInsets.all(10),
+              child: Center(
+                child: Icon(
+                  Icons.star,
+                  color: Colors.red,
+                ),
+              ),
+            ),
+          ],
+        ),
+        Padding(padding: EdgeInsets.all(3)),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Icon(
+                  Icons.call,
+                  color: Colors.blue,
+                ),
+                Text(
+                  'Contract',
+                  style: TextStyle(color: Colors.blue),
+                )
+              ],
+            )
+          ],
+        )
+      ]),
+    );
+  }
+}
